9301dc8773631b2451b6762f1ebe96cdc0e24b7a
fjahangiri <fatemejahangiri1377@gmail.com>
2019-02-21 15:11:13 +0330

    third

--- Main_Board/src/main.cpp
+++ Main_Board/src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
 
+LiquidCrystal lcd(12,11,5,4,3,2);
 void setup() {
   // put your setup code here, to run once:
+  lcd.begin(16,2);
+  lcd.print("hello");
 }
 
 void loop() {
+  lcd.setCursor(0,1);
+  lcd.println("thanks");
   // put your main code here, to run repeatedly:
 }
--- Sensor_Board/src/main.cpp
+++ Sensor_Board/src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
 
+int val;
+int tempPin=1;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+
 }
 
 void loop() {
+  val=analogRead(tempPin);
+  float mv= (val/1024.0)*5000;
+  float cell=mv/10;
+
+  Serial.print("ttt");
+  Serial.print(cell);
+  Serial.println();
+  delay(1000);
   // put your main code here, to run repeatedly:
 }
