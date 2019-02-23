def6fbaed9fae719b422752c39422eb26b329ce6
fjahangiri <fatemejahangiri1377@gmail.com>
2019-02-24 00:25:36 +0330

    debug3

--- Main_Board/src/main.cpp
+++ Main_Board/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
-
+#include <AltSoftSerial.h>
 
 LiquidCrystal lcd(12,11,5,4,3,2);
+AltSoftSerial altSerial;
+
 SoftwareSerial softserial(7,6);//6 RX , 7 TX
 byte buffer[4];
 float num;
@@ -11,7 +13,7 @@ void setup(){
   num=0;
   lcd.begin(16 ,2);               
   Serial.begin(9600);
-  softserial.begin(9600);
+  altSerial.begin(9600);
   lcd.setCursor ( 0 , 1 ); 
 }
 void loop(){
@@ -24,30 +26,10 @@ void loop(){
       }
      
     }     
-      float x = *((float*)(buffer));
+   float x = *((float*)(buffer));
     lcd.println(x);
 
 
-  softserial.listen();
-  if(softserial.available()){
-    char data;
-    while(true){
-      data = softserial.read();
-      if(data == 'e'){
-        break;
-      }
-      if(data == 'u'){
-        lcd.print("cm");
-        lcd.println();
-      }
-      else{
-        //softserial.print(data);
-        lcd.print(data);
-      }
-    }
-    lcd.print("`c");
-    lcd.println(); 
-  }
       delay(100); 
 
 }
